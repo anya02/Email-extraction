4ec3c54505d39fe8744324520901069b3bf2b221
Anya Murphy <anya.murphy@mac.com>
2022-02-03 14:53:27 +0000

    Read & Print text file

--- Email Extraction/Email Extraction/Program.cs
+++ Email Extraction/Email Extraction/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] lines = File.ReadAllLines(@"C:\Users\8184rj\source\repos\Email-extraction\Email Extraction\Email Extraction\Emails.txt");
+
+foreach (string line in lines)
+{
+    Console.WriteLine(line);
+}
